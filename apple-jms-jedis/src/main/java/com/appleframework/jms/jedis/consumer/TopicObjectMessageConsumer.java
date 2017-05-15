f8547545f0a37dd5917e0217b059421f11c4f803
xushaomin <xushaomin@foxmail.com>
2017-05-15 21:24:38 +0800

    新增jedis的queue和topic

---
+++ apple-jms-jedis/src/main/java/com/appleframework/jms/jedis/consumer/TopicObjectMessageConsumer.java
@@ -0,0 +1,89 @@
+package com.appleframework.jms.jedis.consumer;
+
+import javax.annotation.Resource;
+
+import org.apache.log4j.Logger;
+
+import com.appleframework.cache.jedis.factory.PoolFactory;
+import com.appleframework.jms.core.consumer.MessageConusmer;
+import com.appleframework.jms.core.utils.ByteUtils;
+
+import redis.clients.jedis.BinaryJedisPubSub;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+/**
+ * @author Cruise.Xu
+ * 
+ */
+@SuppressWarnings("deprecation")
+public abstract class TopicObjectMessageConsumer extends MessageConusmer<Object> {
+
+	private static Logger logger = Logger.getLogger(TopicObjectMessageConsumer.class);
+
+	@Resource
+	protected PoolFactory poolFactory;
+
+	protected String topic;
+	
+	private BinaryJedisPubSub pubSub = new BinaryJedisPubSub() {
+		@Override
+		public void onMessage(byte[] channel, byte[] message) {
+			processMessage(ByteUtils.fromByte(message));
+		}
+
+		@Override
+		public void onPMessage(byte[] pattern, byte[] channel, byte[] message) {
+			processMessage(ByteUtils.fromByte(message));
+		}
+
+		@Override
+		public void punsubscribe() {
+			super.punsubscribe();
+		}
+
+		@Override
+		public void punsubscribe(byte[]... patterns) {
+			super.punsubscribe(patterns);
+		}
+	};
+
+	protected void init() {
+		String[] topics = topic.split(",");
+		JedisPool jedisPool = poolFactory.getWritePool();
+		Jedis jedis = jedisPool.getResource();
+		try {
+			for (int i = 0; i < topics.length; i++) {
+				final String topicc = topics[i];
+				jedis.psubscribe(pubSub, topicc.getBytes());
+			}
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+		} finally {
+			jedisPool.returnResource(jedis);
+		}
+
+		Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+			public void run() {
+				unsubscribe();
+			}
+		}));
+	}
+	
+	private void unsubscribe() {
+		pubSub.unsubscribe();
+	}
+
+	public void setTopic(String topic) {
+		this.topic = topic.trim().replaceAll(" ", "");
+	}
+
+	public void setPoolFactory(PoolFactory poolFactory) {
+		this.poolFactory = poolFactory;
+	}
+
+	public void destroy() {
+		unsubscribe();
+	}
+
+}
