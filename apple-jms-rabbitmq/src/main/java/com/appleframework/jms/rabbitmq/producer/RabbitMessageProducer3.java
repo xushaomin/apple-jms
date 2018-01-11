2863de4f223785d7025e325fdf426f25ee6e13f1
xushaomin <xushaomin@foxmail.com>
2018-01-11 20:08:56 +0800

    update

---
+++ apple-jms-rabbitmq/src/main/java/com/appleframework/jms/rabbitmq/producer/RabbitMessageProducer3.java
@@ -0,0 +1,63 @@
+package com.appleframework.jms.rabbitmq.producer;
+
+import java.io.Serializable;
+
+import org.apache.log4j.Logger;
+
+import com.appleframework.jms.core.exception.JmsException;
+import com.appleframework.jms.core.exception.MQException;
+import com.appleframework.jms.core.producer.MessageProducer3;
+import com.appleframework.jms.core.utils.ByteUtils;
+import com.rabbitmq.client.AMQP.BasicProperties;
+import com.rabbitmq.client.Channel;
+
+/**
+ * @author Cruise.Xu
+ * 
+ */
+public class RabbitMessageProducer3 implements MessageProducer3 {
+
+	private final static Logger logger = Logger.getLogger(RabbitMessageProducer3.class);
+
+	private Channel channel;
+
+	private BasicProperties props = null;
+
+	public void sendByte(String topic, String exchange, byte[] message) throws JmsException {
+		try {
+			channel.basicPublish(exchange, topic, props, message);
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+			throw new MQException(e);
+		}
+	}
+
+	@Override
+	public void sendObject(String topic, String exchange, Serializable message) throws JmsException {
+		try {
+			channel.basicPublish(exchange, topic, props, ByteUtils.toBytes(message));
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+			throw new MQException(e);
+		}
+	}
+
+	@Override
+	public void sendText(String topic, String exchange, String message) throws JmsException {
+		try {
+			channel.basicPublish(exchange, topic, props, message.getBytes());
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+			throw new MQException(e);
+		}
+	}
+
+	public void setChannel(Channel channel) {
+		this.channel = channel;
+	}
+	
+	public void setProps(BasicProperties props) {
+		this.props = props;
+	}
+
+}
