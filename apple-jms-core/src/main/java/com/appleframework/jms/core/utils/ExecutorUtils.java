e23d955a6be8de974f3835042628af9b02caff42
xushaomin <xushaomin@9zhitx.com>
2019-09-26 11:26:26 +0800

    update

---
+++ apple-jms-core/src/main/java/com/appleframework/jms/core/utils/ExecutorUtils.java
@@ -0,0 +1,26 @@
+package com.appleframework.jms.core.utils;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class ExecutorUtils {
+
+	public static ThreadPoolExecutor newFixedThreadPool(int nThreads, Integer queueCapacity, ThreadFactory threadFactory) {
+		BlockingQueue<Runnable> workQueue = null;
+		if (null == queueCapacity || queueCapacity <= 0) {
+			workQueue = new LinkedBlockingQueue<Runnable>();
+		} else {
+			workQueue = new LinkedBlockingQueue<Runnable>(queueCapacity);
+		}
+		if(null == threadFactory) {
+			return new ThreadPoolExecutor(nThreads, nThreads, 0L, TimeUnit.MILLISECONDS, workQueue);
+		}
+		else {
+			return new ThreadPoolExecutor(nThreads, nThreads, 0L, TimeUnit.MILLISECONDS, workQueue, threadFactory);
+		}
+		
+	}
+}
